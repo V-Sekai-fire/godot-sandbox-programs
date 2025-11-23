69bfd7d8bd2b8560fe448b1cb2e319278115c596
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-11-22 16:39:52 -0800

    Remove wrong pointer.

--- thirdparty/llvm-project/zig.cmd
+++
@@ -1,6 +0,0 @@
-set HERE=%cd%
-mkdir .zig
-pushd .zig
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON -DCMAKE_TOOLCHAIN_FILE=%HERE%\cmake\zig-toolchain.cmake
-cmake --build .
-popd
--- thirdparty/llvm-project/zig.sh
+++
@@ -1,7 +0,0 @@
-TOOLCHAIN=$PWD/cmake/zig-toolchain.cmake
-
-mkdir -p .zig
-pushd .zig
-cmake .. -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN
-make -j8
-popd
